7814b505dbdde42a23f77c9e0283ff93f9a2c19f
vfx <175639080+vfXwq@users.noreply.github.com>
2024-09-07 13:35:33 -0700

    made humanchoice and computer choice functions work

--- index.js
+++ index.js
@@ -4,5 +4,13 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    
+    while (true) {
+        let choice = prompt("Enter your choice: ").toLowerCase();
+        if (choice === "rock" || choice === "paper" || choice === "scissors") {
+            return choice;
+        }
+        else {
+            alert("Incorrect choice, please try again");
+        }
+    }
 }
